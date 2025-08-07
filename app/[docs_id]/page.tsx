25790fc721800499fc8731f3387f46ee1ac6d022
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-08-07 18:41:51 +0900

    dev環境ではローカルのファイルを読むようにする

--- app/[docs_id]/page.tsx
+++ app/[docs_id]/page.tsx
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation";
 import { ChatForm } from "./chatForm";
 import { StyledMarkdown } from "./markdown";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { readFile } from "node:fs/promises"; 
+import { join } from "node:path";
 
 export default async function Page({
   params,
@@ -12,10 +14,17 @@ export default async function Page({
 
   let mdContent: string;
   try {
-    const cfAssets = getCloudflareContext().env.ASSETS;
-    mdContent = await cfAssets!
-      .fetch(`https://assets.local/docs/${docs_id}.md`)
-      .then((res) => res.text());
+    if (process.env.NODE_ENV === 'development') {
+      mdContent = await readFile(
+        join(process.cwd(), "public", "docs", `${docs_id}.md`),
+        "utf-8"
+      );
+    } else {
+	  const cfAssets = getCloudflareContext().env.ASSETS;
+      mdContent = await cfAssets!
+        .fetch(`https://assets.local/docs/${docs_id}.md`)
+        .then((res) => res.text());
+    }
   } catch (error) {
     console.error(error);
     notFound();
