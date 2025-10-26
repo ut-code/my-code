126b223848448b41b411c7ce7eedabdba08477be
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-10-26 18:26:29 +0900

    workerで動くよう修正

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,8 @@
-import { auth } from "@/lib/auth"; // path to your auth file
+import { getAuthServer } from "@/lib/auth";
+import { getPrismaClient } from "@/lib/prisma";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { POST, GET } = toNextJsHandler(auth);
+export const { POST, GET } = toNextJsHandler({
+  handler: async (req) =>
+    (await getAuthServer(await getPrismaClient())).handler(req),
+});
--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,16 @@
-import { PrismaD1 } from "@prisma/adapter-d1";
-import { PrismaClient } from "../generated/prisma/client";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let cloudflareEnv: any;
-try {
-  cloudflareEnv = getCloudflareContext().env;
-} catch {
-  // @better-auth/cli generate を実行する際には initOpenNextCloudflareForDev がセットアップされていない環境になっている
-  cloudflareEnv = {};
+export async function getPrismaClient() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let cloudflareEnv: any;
+  try {
+    cloudflareEnv = (await getCloudflareContext({ async: true })).env;
+  } catch {
+    // @better-auth/cli generate を実行する際には initOpenNextCloudflareForDev がセットアップされていない環境になっている
+    cloudflareEnv = {};
+  }
+
+  return new PrismaClient({ adapter: new PrismaD1(cloudflareEnv.my_code_db) });
 }
-
-const prisma = new PrismaClient({ adapter: new PrismaD1(cloudflareEnv.my_code_db) });
-export default prisma;
