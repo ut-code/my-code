6d37d66d06f615758a3e8dd5ebf888741c049435
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-03-17 22:46:13 +0900

    未使用のaction

--- app/actions/getChatAll.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { getAllChat, initContext } from "@/lib/chatHistory";
-import { PagePath } from "@/lib/docs";
-
-export async function getChatAllWithoutCache(path: PagePath) {
-  const ctx = await initContext();
-  return await getAllChat(path, ctx);
-}
