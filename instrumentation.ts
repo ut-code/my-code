ae747fa714ec1d4806a41b895bc43b5bd0ebde5a
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-04-07 19:20:12 +0900

    instrumentation.tsでsentryをトップレベルimportしない

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,3 @@
-import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
@@ -8,4 +7,8 @@ export async function register() {
   }
 }
 // Capture errors from Server Components, middleware, and proxies
-export const onRequestError = Sentry.captureRequestError;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function onRequestError(e: unknown, r: any, ec: any) {
+  const { captureRequestError } = await import("@sentry/nextjs");
+  captureRequestError(e, r, ec);
+}
