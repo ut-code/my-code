802d9dd8189dc1e8dd55bb610b5d1a2c58abd1a1
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-08 12:52:11 +0900

    add zodErrorsIntegration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,8 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
 Sentry.init({
-  enabled: process.env.NODE_ENV !== 'development',
+  enabled: process.env.NODE_ENV !== "development",
   dsn: process.env.SENTRY_DSN,
-  integrations: [Sentry.extraErrorDataIntegration({ depth: 10 })],
+  integrations: [
+    Sentry.extraErrorDataIntegration({ depth: 10 }),
+    Sentry.zodErrorsIntegration(),
+  ],
   // Adds request headers and IP for users
   sendDefaultPii: true,
 });
