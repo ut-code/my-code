9c80560585d7514755e051fa24d60625d6bafcce
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-05-08 12:25:38 +0900

    add extraErrorDataIntegration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   enabled: process.env.NODE_ENV !== 'development',
   dsn: process.env.SENTRY_DSN,
+  integrations: [Sentry.extraErrorDataIntegration({ depth: 10 })],
   // Adds request headers and IP for users
   sendDefaultPii: true,
 });
