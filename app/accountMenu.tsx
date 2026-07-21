1ecf3828b812aa4f1029d0cd8dc562196ca1ccb5
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-07-21 13:46:27 +0900

    dropdownの実装をdetails/summaryに変更
    labelでは勝手に閉じてしまうため

--- app/accountMenu.tsx
+++ app/accountMenu.tsx
@@ -2,7 +2,7 @@
 
 import { authClient } from "@/lib/auth-client";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export function AutoAnonymousLogin() {
   const { data: session, isPending } = authClient.useSession();
@@ -72,54 +72,47 @@ export function AccountMenu() {
     return <div className="w-10 h-10 skeleton rounded-full"></div>;
   }
 
+  let avatar: ReactNode;
+  let content: ReactNode;
   if (session && !session.user.isAnonymous) {
-    return (
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-8 h-8 rounded-full">
-            <img
-              src={
-                session.user?.image ??
-                `https://avatar.vercel.sh/${session.user?.name}`
-              }
-              alt="user avatar"
-              crossOrigin="anonymous"
-            />
-          </div>
-        </label>
-        <ul
-          tabIndex={0}
-          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a onClick={signout}>ログアウト</a>
-          </li>
-        </ul>
+    avatar = (
+      <div className="w-8 h-8 rounded-full">
+        <img
+          src={
+            session.user?.image ??
+            `https://avatar.vercel.sh/${session.user?.name}`
+          }
+          alt="user avatar"
+          crossOrigin="anonymous"
+        />
       </div>
     );
-  }
-
-  return (
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-8 h-8 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-full h-full"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        </div>
-      </label>
-      <ul className="z-1 shadow-md dropdown-content bg-base-100 rounded-box menu w-64">
+    content = (
+      <>
+        <li>
+          <a onClick={signout}>ログアウト</a>
+        </li>
+      </>
+    );
+  } else {
+    avatar = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-8 h-8"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+        />
+      </svg>
+    );
+    content = (
+      <>
         <li className="menu-title">
           ログインすると、チャット履歴を保存し別のデバイスからもアクセスできるようになります。
         </li>
@@ -160,7 +153,16 @@ export function AccountMenu() {
             </li>
           </>
         )}
+      </>
+    );
+  }
+
+  return (
+    <details className="dropdown dropdown-end">
+      <summary className="btn btn-ghost btn-circle avatar">{avatar}</summary>
+      <ul className="z-1 shadow-md dropdown-content bg-base-100 rounded-box menu w-64">
+        {content}
       </ul>
-    </div>
+    </details>
   );
 }
