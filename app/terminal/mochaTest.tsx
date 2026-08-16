59430ebad9d0e428dc7d1458b93c6759068decd8
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2026-08-17 02:52:10 +0900

    テストを /about/runtime に移動

--- app/terminal/mochaTest.tsx
+++ app/terminal/mochaTest.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { Heading } from "@/markdown/heading";
 import "mocha/mocha.css";
-import { Fragment, useEffect, useRef, useState } from "react";
-import { langConstants, RuntimeLang } from "@my-code/runtime/languages";
-import { ReplTerminal } from "./repl";
-import { EditorComponent } from "./editor";
-import { ExecFile } from "./exec";
-import { useTerminal } from "./terminal";
+import { useEffect, useRef, useState } from "react";
+import { RuntimeLang } from "@my-code/runtime/languages";
 import {
   RUNTIME_TIMEOUTS,
   waitForRuntimeReady,
@@ -15,140 +10,14 @@ import {
 import { replTests } from "@my-code/runtime/tests/repl";
 import { fileExecutionTests } from "@my-code/runtime/tests/fileExecution";
 import { useRuntimeAll } from "@my-code/runtime/context";
-import { captureException } from "@sentry/nextjs";
-
-import { sampleConfig, SampleConfig } from "./sampleConfig";
 import { DaisyInfoIcon } from "@/daisyAlertIcon";
-
-export default function RuntimeTestPage() {
-  return (
-    <div className="p-4 mx-auto w-full max-w-docs">
-      <Heading level={1}>Runtime Test Page</Heading>
-
-      <Heading level={2}>REPLとコード実行のサンプル</Heading>
-      {/* name of each tab group should be unique */}
-      <div className="tabs tabs-box">
-        {Object.entries(sampleConfig).map(([lang, config]) => (
-          <Fragment key={lang}>
-            <input
-              type="radio"
-              name="runtime-sample-tabs"
-              className="tab"
-              aria-label={lang}
-            />
-            <div className="tab-content border-base-300 bg-base-100">
-              <RuntimeSample lang={lang as RuntimeLang} config={config} />
-            </div>
-          </Fragment>
-        ))}
-      </div>
-
-      <Heading level={2}>Xterm.js Colors</Heading>
-      <AnsiColorSample />
-
-      <button
-        className="btn mt-4"
-        onClick={() => {
-          throw new Error("Sentry Test Error");
-        }}
-      >
-        Sentry Test Error
-      </button>
-
-      <Heading level={2}>自動テスト</Heading>
-      <MochaTest />
-    </div>
-  );
-}
-
-function RuntimeSample({
-  lang,
-  config,
-}: {
-  lang: RuntimeLang;
-  config: SampleConfig;
-}) {
-  return (
-    <div className="flex flex-col">
-      {config.repl && (
-        <ReplTerminal
-          terminalId="1"
-          language={langConstants(lang)}
-          initContent={config.replInitContent}
-        />
-      )}
-      {config.editor &&
-        Object.entries(config.editor).map(([filename, initContent]) => (
-          <EditorComponent
-            key={filename}
-            language={langConstants(lang)}
-            filename={filename}
-            initContent={initContent}
-          />
-        ))}
-      {config.exec && (
-        <ExecFile
-          filenames={config.exec}
-          language={langConstants(lang)}
-          content=""
-        />
-      )}
-      {config.readonlyFiles?.map((filename) => (
-        <EditorComponent
-          key={filename}
-          language={langConstants(lang)}
-          filename={filename}
-          initContent=""
-          readonly
-        />
-      ))}
-    </div>
-  );
-}
-
-function AnsiColorSample() {
-  const { terminalRef, terminalInstanceRef } = useTerminal({
-    getRows: () => 6,
-    onReady: () => {
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[0;${30 + i}m${30 + i}`);
-      }
-      terminalInstanceRef.current!.write("\x1b[0m\n");
-      terminalInstanceRef.current!.write("\x1b[1m1;");
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[1;${30 + i}m${30 + i}`);
-      }
-      terminalInstanceRef.current!.write("\x1b[0m\n");
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[0;${90 + i}m${90 + i}`);
-      }
-      terminalInstanceRef.current!.write("\x1b[0m\n");
-      terminalInstanceRef.current!.write("\x1b[1m1;");
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[1;${90 + i}m${90 + i}`);
-      }
-      terminalInstanceRef.current!.write("\x1b[0m\n");
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[0;${40 + i}m${40 + i}`);
-      }
-      terminalInstanceRef.current!.write("\x1b[0m\n");
-      for (let i = 0; i <= 7; i++) {
-        terminalInstanceRef.current!.write(`\x1b[0;${100 + i}m${100 + i}`);
-      }
-    },
-  });
-  return (
-    <div className="bg-base-300 border border-accent border-2 shadow-md m-2 p-4 pr-1 rounded-box relative h-max">
-      <div ref={terminalRef} />
-    </div>
-  );
-}
+import { captureException } from "@sentry/nextjs";
 
 function handleRuntimeError(error: unknown) {
   captureException(error);
 }
 
-function MochaTest() {
+export function MochaTest() {
   const runtimeAll = useRuntimeAll();
   const runtimeRef = useRef(runtimeAll);
   for (const lang of Object.keys(runtimeAll) as RuntimeLang[]) {
